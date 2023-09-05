089742809cc26fa6a60014a96e3258c69dfcf4c0
abdelbasset4 <benatmani1999@gmail.com>
2023-09-05 21:13:24 +0100

    update generate and refresh token function

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-exports.generateToken = (payload,expired)=> jwt.sign({userId:payload},process.env.JWT_SECRET,{expiresIn:expired})
+
+exports.generateToken = (payload)=> jwt.sign(payload,process.env.JWT_SECRET,{ expiresIn: process.env.JWT_EXPIRATION_LOGIN })
 
 exports.generateRefreshToken = (payload)=> jwt.sign({userId:payload},process.env.JWT_REFRESH_SECRET,{expiresIn:process.env.JWT_REFRESH_EXPIRE})
 
